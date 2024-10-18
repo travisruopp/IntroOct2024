4da2bebe30cd9f586a51ae8c39f5bd1888d6861f
Travis Ruopp <travisruopp@yahoo.com>
2024-10-18 14:50:38 -0400

    Done with week 1

--- week2/SoftwareSolution/Software.Api/Catalog/CatalogController.cs
+++ week2/SoftwareSolution/Software.Api/Catalog/CatalogController.cs
@@ -1,19 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Marten;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Software.Api.Catalog;
 
-public class CatalogController : ControllerBase
+public class CatalogController(IDocumentSession session) : ControllerBase
 {
     [HttpPost("/catalog")]
     public async Task<ActionResult> AddSoftwareToCatalogAsync([FromBody] CatalogCreateModel request)
     {
         var response = new CatalogResponseModel()
         {
-            Id = Guid.Empty,
+            Id = Guid.NewGuid(),
             IsOpenSource = request.IsOpenSource,
             Title = request.Title,
             Vendor = request.Vendor,
         };
+        var thingToSave = new CatalogEntity()
+        {
+            Id = response.Id,
+            IsOpenSource = response.IsOpenSource,
+            Title = response.Title,
+            Vendor = response.Vendor,
+        };
+        session.Store(thingToSave);
+        await session.SaveChangesAsync();
 
         return Ok(response);
     }
@@ -33,4 +43,10 @@ public record CatalogResponseModel
     public string Vendor { get; set; } = string.Empty;
     public bool IsOpenSource { get; set; }
 }
-
+public class CatalogEntity
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Vendor { get; set; } = string.Empty;
+    public bool IsOpenSource { get; set; }
+}
